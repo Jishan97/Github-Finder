9a493bd95cc7fe00461d7e76733c264ac9b82791
Jishan <shaikh.jishan1997@gmail.com>
2021-02-18 23:23:09 +0000

    build files

---
+++ build/precache-manifest.faf03e2ece741f07dff68ccc21f4a07e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3ad47e6303438af37821b3b2b514797",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b96271ce6361b74cc5e",
+    "url": "/static/css/main.bc1a1419.chunk.css"
+  },
+  {
+    "revision": "51561d15f603e1caa008",
+    "url": "/static/js/2.e2ebb101.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e2ebb101.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b96271ce6361b74cc5e",
+    "url": "/static/js/main.42be71b5.chunk.js"
+  },
+  {
+    "revision": "b2de36bd375bba6a2e4f",
+    "url": "/static/js/runtime-main.d76a22e0.js"
+  }
+]);
